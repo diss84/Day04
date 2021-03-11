a14e3c2633878aa6916a7a03560da2a5baa5fe1e
Mirela <adisserdarevic@gmail.com>
2021-03-11 18:51:50 +0100

    Added file index.js

---
+++ ex03/src/index.js
@@ -0,0 +1,14 @@
+function main (){
+
+    var myRes = 16;
+
+        // only change code below this line
+     var myRes = 16;
+     var y = myRes--;
+     
+    // Only change code above this line
+return myRes;
+}
+
+console.log(main());
+module.exports = main;

7f77046dd4bba8cbc3626b0741f51aecfe1933ed
Mirela <adisserdarevic@gmail.com>
2021-03-11 21:37:06 +0100

    Added file index.js

---
+++ ex10/src/index.js
@@ -0,0 +1,19 @@
+function main(){
+   
+    
+    // Only change code below this line
+    var myFirstName = "Adis"
+    var myLastName = "Serdarevic"
+    var myHomeTown = "Tuzla"
+
+
+    // Only change code above this line
+
+    return {myFirstName,
+    myLastName,
+    myHomeTown};
+}
+  
+console.log(main()); // change this line
+
+module.exports = main;

4c8a04cf420e0eda97e75de9ff1a3acd39e291ef
Mirela <adisserdarevic@gmail.com>
2021-03-11 20:53:01 +0100

    Added file index.js

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -2,14 +2,16 @@ function main(numOne, numTwo){
    
     // Only change code below this line
 
-var numOne = 45;
-var numTwo = 7;
-var reminder  = numOne % numTwo
+var numOne;
+var numTwo;
+var reminder = numOne % numTwo
     // Only change code above this line
 
     return reminder;
 }
   
-console.log(main()); // change this line
+console.log(main(5, 2)); // change this line
+console.log(main(15, 4)); // change this line
+console.log(main(102, 25)); // change this line
 
 module.exports = main;
